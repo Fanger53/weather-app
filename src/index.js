cb89e8e107de406cd413fae5b17591571a7ccb67
Leonardo <fanger53@gmail.com>
2021-05-20 17:25:30 -0500

    add change temperature values and weather color background

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import UI from './modules/UI';
 
 const searchInput = document.getElementById('search-input');
 const searchButon = document.getElementById('search');
+const switchButton = document.getElementById('switch-temperature');
 
 const weather = new Weather();
 const ui = new UI();
@@ -11,12 +12,20 @@ const ui = new UI();
 const fetchWeather = async () => {
   const data = await weather.getWeather();
   ui.render(data);
+  ui.changeTemp(data);
+  ui.changeTempColor(data);
 };
 
 searchButon.addEventListener('click', () => {
   const input = searchInput.value;
   weather.chooseLocation(input);
   fetchWeather();
+});
+
+switchButton.addEventListener('click', async () => {
+  const data = await weather.getWeather();
+  fetchWeather();
+  ui.changeTemp(data);
 });
 
 document.addEventListener('DOMContentLoaded', fetchWeather);
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-concat */
 export default class UI {
   constructor() {
     this.location = document.getElementById('location');
@@ -5,14 +6,40 @@ export default class UI {
     this.desc = document.getElementById('description');
     this.humidity = document.getElementById('humidity');
     this.wind = document.getElementById('wind');
+    this.tempSpan = document.getElementById('temp-span');
+    this.bodyBg = document.getElementById('body');
   }
 
   render(weather) {
     this.location.textContent = weather.name;
-    // eslint-disable-next-line no-useless-concat
-    this.temperature.textContent = `${Math.round(weather.main.temp)} °C` + ` / ${Math.round((weather.main.temp * 9) / 5 + 32)} °F`;
+    this.temperature.textContent = `${Math.round(weather.main.temp)}` + 'C°';
     this.desc.textContent = weather.weather[0].description;
     this.humidity.textContent = `${weather.main.humidity} %`;
     this.wind.textContent = `${weather.wind.speed} m/s`;
+  }
+
+  changeTemp(weather) {
+    if (this.tempSpan.textContent === 'C') {
+      this.tempSpan.textContent = 'F';
+    } else if (this.tempSpan.textContent === 'F') {
+      this.temperature.textContent = `${Math.round((weather.main.temp * 9) / 5 + 32)} °F`;
+      this.tempSpan.textContent = '';
+    } else {
+      this.tempSpan.textContent = 'C';
+      this.temperature.textContent = `${Math.round(weather.main.temp)}` + 'C°';
+    }
+  }
+
+  changeTempColor(weather) {
+    if (`${Math.round(weather.main.temp)}` >= 15 && `${Math.round(weather.main.temp)}` <= 22) {
+      this.bodyBg.classList.add('warm');
+      this.bodyBg.classList.remove('cold', 'hot');
+    } else if (`${Math.round(weather.main.temp)}` <= 15) {
+      this.bodyBg.classList.add('cold');
+      this.bodyBg.classList.remove('warm', 'hot');
+    } else if (`${Math.round(weather.main.temp)}` >= 23) {
+      this.bodyBg.classList.add('hot');
+      this.bodyBg.remove.className('cold', 'warm');
+    }
   }
 }
